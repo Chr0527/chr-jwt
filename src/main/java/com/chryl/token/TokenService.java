cde50d3c7db58a3ffb451f50490e4d3c726004aa
Chr0527 <1015670600@qq.com>
2019-07-19 16:20:03 +0800

    init

--- src/main/java/com/chryl/token/TokenService.java
+++ src/main/java/com/chryl/token/TokenService.java
@@ -21,6 +21,14 @@ public class TokenService {
                  * 密码作为秘钥 不太好,建议自己定义
                  */
                 .sign(Algorithm.HMAC256(user.getUserPassword()));//password作为token的秘钥
+        /**
+         * Token认证的五点认识:
+         * 一个Token就是一些信息的集合；
+         * 在Token中包含足够多的信息，以便在后续请求中减少查询数据库的几率；
+         * 服务端需要对cookie和HTTP Authrorization Header进行Token信息的检查；
+         * 基于上一点，你可以用一套token认证代码来面对浏览器类客户端和非浏览器类客户端；
+         * 因为token是被签名的，所以我们可以认为一个可以解码认证通过的token是由我们系统发放的，其中带的信息是合法有效的；
+         */
         return token;
     }
 
