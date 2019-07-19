5d6a69ce2820d6d442e80de400b70ecfb4760154
Chr0527 <1015670600@qq.com>
2019-07-19 11:36:54 +0800

    Initial commit

---
+++ src/main/java/com/chryl/ChrJwtApplication.java
@@ -0,0 +1,13 @@
+package com.chryl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrJwtApplication.class, args);
+	}
+
+}
